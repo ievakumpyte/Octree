3443e712c63d878d9c51d53b38cde0b03588b0c1
ievakumpyte <ieva.kumpytee@gmail.com>
2023-01-24 20:38:49 +0200

    Octree

---
+++ Octree/Assets/CreateOctree.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateOctree : MonoBehaviour
+{
+    public GameObject[] worldObjects;
+    public int nodeMinSize = 5;
+    Octree otree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        otree = new Octree(worldObjects, nodeMinSize);
+    }
+
+    void OnDrawGizmos()
+    {
+        if (Application.isPlaying)
+        {
+            otree.rootNode.Draw();
+            
+        }
+    }
+}
